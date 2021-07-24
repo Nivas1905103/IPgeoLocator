7dc8bf59c39e347789a5ba25c3a210312945b62d
Nivas1905103 <87891518+Nivas1905103@users.noreply.github.com>
2021-07-24 16:57:50 +0530

    Update Menu.py

--- core/Menu.py
+++ core/Menu.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # encoding: UTF-8
 
-
+__author__   = 'nivas'
+__version__  = '2.0.4'
+__year__     = '2020021'
 
 from argparse import RawTextHelpFormatter
 import argparse, os
