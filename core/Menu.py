0359b4cc9f048930e5736f85ac54aeca4a01f16b
Nivas1905103 <87891518+Nivas1905103@users.noreply.github.com>
2021-07-24 17:13:13 +0530

    Update Menu.py

--- core/Menu.py
+++ core/Menu.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # encoding: UTF-8
 
+
 __author__   = 'nivas'
+__twitter__  = '@nivas'
 __version__  = '2.0.4'
 __year__     = '2020-2021'
+
 
 from argparse import RawTextHelpFormatter
 import argparse, os
@@ -12,13 +15,13 @@ from core.Logger import Red
 
 
 banner = """
-{0}
+{0} 
 
 {1} Retrieve IP Geolocation information from ip-api.com
 {1} Copyright (c) {2} {3} ({4})
 {1} ip-api.com service will automatically ban any IP addresses doing over 150 requests per minute.
 
-""".format(Red('IPGeolocation ' + __version__), Red('--['), __year__, __author__)
+""".format(Red('IPGeolocation ' + __version__), Red('--['), __year__, __author__, __twitter__)
 
 
 def checkFileRead(filename):
@@ -39,8 +42,8 @@ def checkFileWrite(filename):
         return filename
     else:
         raise argparse.ArgumentTypeError("Unable to write to {} file (Insufficient permissions).".format(filename))
-
-
+    
+    
 def checkProxyUrl(url):
     """Check if proxy url is valid"""
     url_checked = urlparse(url)
@@ -50,78 +53,78 @@ def checkProxyUrl(url):
 
 
 parser = argparse.ArgumentParser(description=banner, formatter_class=RawTextHelpFormatter)
-
+    
 #pick target/s
-parser.add_argument('-m', '--my-ip',
+parser.add_argument('-m', '--my-ip',  
                     dest='myip',
-                    action='store_true',
+                    action='store_true', 
                     help='Get Geolocation info for my IP address.')
 
-parser.add_argument('-t', '--target',
+parser.add_argument('-t', '--target',  
                     help='IP Address or Domain to be analyzed.')
 
-parser.add_argument('-T', '--tlist',
+parser.add_argument('-T', '--tlist', 
                     metavar='file',
-                    type=checkFileRead,
+                    type=checkFileRead, 
                     help='A list of IPs/Domains targets, each target in new line.')
 
 
 #user-agent configuration
-parser.add_argument('-u', '--user-agent',
-                    metavar='User-Agent',
+parser.add_argument('-u', '--user-agent', 
+                    metavar='User-Agent', 
                     dest='uagent',
-                    default='IP2GeoLocation {}'.format(__version__),
+                    default='IP2GeoLocation {}'.format(__version__), 
                     help='Set the User-Agent request header (default: IP2GeoLocation {}).'.format(__version__))
 
-parser.add_argument('-U', '--ulist',
-                    metavar='file',
-                    type=checkFileRead,
+parser.add_argument('-U', '--ulist', 
+                    metavar='file', 
+                    type=checkFileRead, 
                     help='A list of User-Agent strings, each string in new line.')
 
 
 #misc options
-parser.add_argument('-g',
-                    action='store_true',
+parser.add_argument('-g', 
+                    action='store_true', 
                     help='Open IP location in Google maps with default browser.')
 
-parser.add_argument('--noprint',
-                    action='store_true',
+parser.add_argument('--noprint', 
+                    action='store_true', 
                     help='IPGeolocation will print IP Geolocation info to terminal. It is possible to tell IPGeolocation not to print results to terminal with this option.')
 
-parser.add_argument('-v', '--verbose',
-                    action='store_true',
+parser.add_argument('-v', '--verbose', 
+                    action='store_true', 
                     help='Enable verbose output.')
 
-parser.add_argument('--nolog',
-                    action='store_true',
+parser.add_argument('--nolog', 
+                    action='store_true', 
                     help='IPGeolocation will save a .log file. It is possible to tell IPGeolocation not to save those log files with this option.')
 
 
 #anonymity options
-parser.add_argument('-x', '--proxy',
-                    type=checkProxyUrl,
+parser.add_argument('-x', '--proxy', 
+                    type=checkProxyUrl, 
                     help='Setup proxy server (example: http://127.0.0.1:8080)')
 
-parser.add_argument('-X', '--xlist',
-                    metavar='file',
-                    type=checkFileRead,
+parser.add_argument('-X', '--xlist', 
+                    metavar='file', 
+                    type=checkFileRead, 
                     help='A list of proxies, each proxy url in new line.')
 
 
 #export options
-parser.add_argument('-e', '--txt',
-                    metavar='file',
-                    type=checkFileWrite,
+parser.add_argument('-e', '--txt', 
+                    metavar='file', 
+                    type=checkFileWrite, 
                     help='Export results.')
 
-parser.add_argument('-ec', '--csv',
-                    metavar='file',
-                    type=checkFileWrite,
+parser.add_argument('-ec', '--csv', 
+                    metavar='file', 
+                    type=checkFileWrite, 
                     help='Export results in CSV format.')
 
-parser.add_argument('-ex', '--xml',
-                    metavar='file',
-                    type=checkFileWrite,
+parser.add_argument('-ex', '--xml', 
+                    metavar='file', 
+                    type=checkFileWrite, 
                     help='Export results in XML format.')
 
 
